0a68e8888a3130e8ebaabedcec07052d14f3a453
Aturan <wioanfe@gmail.com>
2014-06-13 15:57:23 +0800

    update

--- demo.php
+++ demo.php
@@ -2,9 +2,50 @@
 <html lang="zh-cn">
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<script type="text/javascript" src="./jquery.min.js"></script>
+	<script type="text/javascript" src="./HTML5Uploader.js?<?= time(); ?>"></script>
+	<script type="text/javascript" src="./makeThumb.js?<?= time(); ?>"></script>
+	<title>HTML5Uploader.js</title>
 </head>
 <body>
-<embed type="application/x-shockwave-flash"pluginspage="http://www.macromedia.com/go/getflashplayer" src="http://player.youku.com/player.php/sid/XNzAyMzcyNTY4_ev_1/v.swf" width="420" height="280" align="none" wmode="transparent" play="true" loop="false" menu="false" allowscriptaccess="never" allowfullscreen="true" />
+<?php
+require_once("./qiniu/http.php");
+require_once("./qiniu/io.php");
+require_once("./qiniu/rs.php");
+
+$accessKey = '_j1zpW84suEITz3sWTmXBZSXfWf33UJ4Wb2vUepP';
+$secretKey = 'tSwQjgPwNU9vk5oCgXGXrvFbyz1E0Vljbe3wkcz4';
+Qiniu_setKeys($accessKey, $secretKey);
+$putPolicy = new Qiniu_RS_PutPolicy('atuploadtest');
+$token = $putPolicy->Token(null);
+?>
+<input type="file" name="file" id="file" />
+<input type="hidden" name="url" id="url" />
+<div id="progress">进度：<span></span></div>
+<div id="img"></div>
+
+<script type="text/javascript">
+	var uploader = new Uploader({
+		trigger: '#file',
+		action: 'http://up.qiniu.com',
+		data: function() {
+			return {
+				token: '<?= $token; ?>',
+				key: 'demo/' + Date.now()
+			};
+		},
+		progress: function(e, now, total, rate) {
+			$('#progress span').text(rate+'%');
+		},
+		success: function(data) {
+			var url = 'http://atuploadtest.qiniudn.com/' + data.key;
+			$('#url').val(url);
+			$('#img').html('<img style="max-width: 100%;" src="'+url+'" />');
+		},
+		error: function() {
+			alert(333);
+		}
+	});
+</script>
 </body>
 </html>
